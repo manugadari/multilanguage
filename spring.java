f2e5e535500f3eee1d9e0b1f2f8f39b1a134321e
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-06-04 13:14:47 +0530

    Create spring.java

---
+++ spring.java
@@ -0,0 +1,24 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
