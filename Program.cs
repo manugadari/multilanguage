4c1d1e69e3a8e1a084f22db99ca28cf13b7528bc
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-06-04 13:19:07 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
